f05d6a6c2a7fe71d831075d9bea1ec27c53337ce
SR <sr@SR-X1.6th>
2019-01-16 19:44:28 +0100

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+     new webpack.HotModuleReplacementPlugin()
+  ]
+};
